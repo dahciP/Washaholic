3599eae86339d26fb1e03945e4b21aca07c57863
dahciP <chinmaypichad@gmail.com>
2022-11-25 12:29:06 +0530

    scrapping

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,18 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const scrape = require("./api/controllers/scrape");
+// const Blog=require("./api/models/blog.model");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
-app.use(
-	"/api/public/uploads",
-	express.static(path.join(__dirname, "/api/public/uploads"))
-);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());	
+
 
 mongoose
 	.connect("mongodb+srv://admin-roshan:Test123@cluster0.s4p00fz.mongodb.net/?retryWrites=true&w=majority", {
@@ -35,17 +34,8 @@ mongoose
 		console.error(err.message);
 	});
 
-app.use("/customers", require("./api/routes/customer.routes"));
-app.use("/moderators", require("./api/routes/moderator.routes"));
-app.use("/packages", require("./api/routes/package.routes"));
-app.use("/users", require("./api/routes/common.routes"));
-app.use("/blog", require("./api/routes/blog.routes"));
-app.use("/feedbacks", require("./api/routes/feedback.routes"));
-app.use("/payment", require("./api/routes/payment.routes"));
-app.use("/order", require("./api/routes/order.routes"));
-
 app.get("/", (req, res) => {
-	res.status(202).cookie("<h3>Washaholic API 2022</h3>").send("cookie is initialised");
+	res.send("<h3>WASHAHOLIC</h3>");
 });
 
 app.get("/scrape",async (req, res) => {
@@ -57,3 +47,17 @@ app.get("/scrape",async (req, res) => {
 		data:data
 	});
 })
+
+app.use(
+	"/api/public/uploads",
+	express.static(path.join(__dirname, "/api/public/uploads"))
+);
+
+app.use("/customers", require("./api/routes/customer.routes"));
+app.use("/moderators", require("./api/routes/moderator.routes"));
+app.use("/packages", require("./api/routes/package.routes"));
+app.use("/users", require("./api/routes/common.routes"));
+app.use("/blog", require("./api/routes/blog.routes"));
+app.use("/feedbacks", require("./api/routes/feedback.routes"));
+app.use("/payment", require("./api/routes/payment.routes"));
+app.use("/order", require("./api/routes/order.routes"));
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import BlogView from '../components/blog/BlogView'
+import React,{useState,useEffect} from 'react'
+// import BlogView from '../components/blog/BlogView'
 import Navbar from '../components/nav/Navbar'
+import axios from 'axios'
+import '../styles/blog.css'
 
 const Blog = () => {
+    const [blogs,setBlogs] = useState([])
+    useEffect(() => {
+        const fetchBlogs = async () => {
+        axios.get('http://localhost:5000/scrape')
+        .then(res => {
+            setBlogs(res.data.data)
+            console.log(res.data.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })}
+        fetchBlogs()
+    },[])
+
+        // const updateBlogs = async () => {
+        // axios.post('http://localhost:5000/blog/update', blogs[0])
+        // .then(res => {
+        //     console.log(res)
+        // }
+        // )
+        // .catch(err => {
+        //     console.log(err)
+        // })}
+        // updateBlogs()
+    // },[])
+
+
     return (
         <div>
             <Navbar/>
-            <BlogView/>
+
+            <div className="blog1-container">
+
+            { blogs && blogs.map(blog => (
+                <div key={blog.id} className='blog1'>
+                    <h1>{blog.title}</h1>
+                    <p>{blog.text}</p>
+                </div>
+            ))}
+
+            </div>
+            
         </div>
     )
 }
